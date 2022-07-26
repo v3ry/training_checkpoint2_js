b0ee9cc877e194b7ec7047130646ba38f923b5d9
v3ry <v3ryfr@gmail.com>
2022-07-26 10:15:01 +0200

    adding api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import GameList from './components/GameList';
 
 function App() {
   return (
     <div className="App">
       <Header name="Tout sur les jeux"/>
+      <GameList/>
     </div>
   );
 }
--- src/components/GameList.jsx
+++ src/components/GameList.jsx
@@ -1,7 +1,21 @@
-function GameList() {
-    return (
-      <div className="Header">
+import axios from "axios";
+import React,{useState,useEffect} from "react";
 
+function GameList() {
+    const [apiData, setApiData] = useState([])
+
+      useEffect(() => {
+        axios
+        .get('https://apis.wilders.dev/wild-games/games/')
+        .then(data => setApiData(data.data))
+        //gestion des erreurs
+        .catch(error => console.warn('Authorization failed : ' + error.message));
+      },[])
+
+    return (
+      <div className="GameList">
+
+        {console.log(apiData)}
       </div>
     );
   }
