60f7795592dfd8725c9036405751b0bf549df6d3
v3ry <v3ryfr@gmail.com>
2022-07-26 11:05:28 +0200

    adding remove shit game button

--- src/components/GameList.jsx
+++ src/components/GameList.jsx
@@ -4,19 +4,33 @@ import Game from "./Game";
 
 function GameList() {
     const [apiData, setApiData] = useState([])
+    const [filter, setFilter] = useState(false)
+    useEffect(() => {
+    axios
+    .get('https://apis.wilders.dev/wild-games/games/')
+    .then(data => setApiData(data.data))
+    //gestion des erreurs
+    .catch(error => console.warn('Authorization failed : ' + error.message));
+    },[])
 
-      useEffect(() => {
-        axios
-        .get('https://apis.wilders.dev/wild-games/games/')
-        .then(data => setApiData(data.data))
-        //gestion des erreurs
-        .catch(error => console.warn('Authorization failed : ' + error.message));
-      },[])
+    const removeGame = () => {
+        setFilter(!filter)
+    }
+    const filterList = (val) => {
+        console.log();
+        if (filter === true){
+            return val.rating>4.5 ? true : false
+        }else{
+            return true
+        }
+    }
 
     return (
       <div className="GameList">
-        {/* {console.log(apiData)} */}
-        {apiData && apiData.map((game,index)=>(
+        <button onClick={removeGame}>Remove Shit Game</button>
+        {apiData && apiData
+        .filter((val) => filterList(val))
+        .map((game,index)=>(
             <Game key={"list"+index} val={game} />
             // console.log(game)
         ))}
